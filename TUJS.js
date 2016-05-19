234eedddf50faa061865e18809dcd0f7036dd93b
Kyler Love <tue94528@temple.edu>
2016-05-19 11:53:37 -0400

    working on table query optimization

--- TUJS.js
+++ TUJS.js
@@ -9,11 +9,11 @@
   JQscript.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(JQscript);
 
-
   var Tblscript = document.createElement('script');
   Tblscript.src = "tableController.js";
   Tblscript.type = 'text/javascript';
   document.getElementsByTagName('head')[0].appendChild(Tblscript);
+
 
 
   function define_TUJS() {
@@ -35,7 +35,7 @@
 
     // About
     TUJS.ver = function() {
-      console.log(" Version: 0.3.1\n Authors: \n 'Kyler Love'\n Began: '11 March 2016' \n\n'Rob Zahorchak'\n Began: '11 May 2016'");
+      console.log(" Version: 0.3.3\n Authors: \n\n 'Kyler Love'\n Began: '11 March 2016' \n\n'Rob Zahorchak'\n Began: '11 May 2016'");
     };
 
     //greet people cause its nice
@@ -46,171 +46,128 @@
 
     //Tracey's take on current events in the world
     TUJS.The_Tracey_Harrison_Manifesto = function() {
-      var NothingMatters = "<div style='Font-size: 200px;'>NOTHING MATTERS<div>";
-      var bod = document.querySelector('body');
-      bod.innerHTML = NothingMatters;
-    };
+      var NothingMatters = "<div style='Font-size: 60px;'> NOTHING MATTERS! <div>";
+      var bod = document.querySelectorAll('a, div');
 
-    //the calc function takes in 3 or 4 arguments and will perform the
-    //passed in function
-    TUJS.Calc = function(a, b, Operator, OutputElem) {
+  setInterval(function(){
+    for (var variable in bod) {
+      console.log(bod);
+      var doc =  bod[variable];
+     doc.innerHTML += NothingMatters;
+   }}, 3000);
 
-      //repurpose OutputElem into the object
-      OutputElem = TUJS.E(OutputElem);
+      };
 
-      var A = parseFloat(TUJS.E(a).value);
-      var B = parseFloat(TUJS.E(b).value);
+      //the calc function takes in 3 or 4 arguments and will perform the
+      //passed in function
+      TUJS.Calc = function(a, b, Operator, OutputElem) {
 
-      var C = 0;
-      switch (Operator){
-        case "+":
-        C = A + B;
-        break;
-        case "-":
-        C = A - B;
-        break;
-        case "*":
-        C = A * B;
-        break;
-        case "/":
-        C = A / B;
-        break;
-        case "%":
-        C = A % B;
-        break;
-      }
-      if(OutputElem === null){
-        console.log(C);
-        return C;
+        //repurpose OutputElem into the object
+        OutputElem = TUJS.E(OutputElem);
 
-      }else{
-        OutputElem.value = C;
-        OutputElem.innerHTML = C;
-      }
+        var A = parseFloat(TUJS.E(a).value);
+        var B = parseFloat(TUJS.E(b).value);
 
-    };
-
-    //turn the innerHTML of any element into a clock
-    TUJS.AddClock = function(elem) {
-      var tag = TUJS.E(elem);
-      var now = new Date();
-      tag.innerHTML = now.toLocaleTimeString();
-    };
-
-    //bool if the obj is found inside the Element(elem) is in passed array
-    TUJS.Contains = function(elem, obj){
-      var a = TUJS.E(elem).innerHTML;
-      var i = a.length;
-      while (i--) {
-        console.log(a[i]);
-        if (a[i] === obj) {
-          console.log('true');
-          return true;
+        var C = 0;
+        switch (Operator){
+          case "+":
+          C = A + B;
+          break;
+          case "-":
+          C = A - B;
+          break;
+          case "*":
+          C = A * B;
+          break;
+          case "/":
+          C = A / B;
+          break;
+          case "%":
+          C = A % B;
+          break;
         }
-      }
-      console.log('false');
-      return false;
-    };
+        if(OutputElem === null){
+          console.log(C);
+          return C;
 
-    //will return an array of the window size of the screen you are on
-    TUJS.getWindowSize = function() {
-      var myWidth = 0, myHeight = 0;
-
-      if( typeof( window.innerWidth ) == 'number' ) {
-        //everything outside of IE
-        myWidth = window.innerWidth;
-        myHeight = window.innerHeight;
-      } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
-        //IE 6+ in 'standards compliant mode'... whatever that means
-        myWidth = document.documentElement.clientWidth;
-        myHeight = document.documentElement.clientHeight;
-      } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
-        //IE 4 compatible... blow it up cause you should never use it
-        myWidth = document.body.clientWidth;
-        myHeight = document.body.clientHeight;
-      }
-      return [ myWidth, myHeight ];
-    };
-
-
-    //returns the difference of two dates (in Days) as inputs
-    TUJS.DateDifference = function(date1, date2, otpt){
-      otpt = TUJS.E(otpt);
-      date1 = TUJS.E(date1).value;
-      date2 = TUJS.E(date2).value;
-
-      date1 = Date.parse(date1);
-      date2 = Date.parse(date2);
-      var timeDiff = Math.abs(date2 - date1);
-      var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
-      otpt.innerHTML = diffDays + " Days";
-    };
-
-    //object call to build a table object form
-    //the tableController js page
-    TUJS.tableController = function() {
-      var tbl = new Table();
-      return tbl;
-    };
-
-
-    //Armond, Kyler's, and Rob's gridview auto filter
-    TUJS.Search_Gridview = function(strKey, strGV, column) {
-        column++;
-        //string of what is typed in
-        //console.log("Before: "+strKey.value.toLowerCase());
-        var strData = strKey.value.toLowerCase().split(" ");
-        // console.log(strData);
-
-        //raw data from table
-        var tblData = document.getElementById(strGV);
-        var table = [];
-        var colInfo = [];
-        var styleDisplay = "";
-        // i is each row in the table.
-        for (var i = 0; i < tblData.rows.length; i++) {
-
-            //all info in row[i]
-            var rowData = tblData.rows[i].innerHTML;
-
-            //splits row and makes arrays ex instead of <td>a</td>....
-            var rowInfo = rowData.split("<td>");
-            for (var j = 1; j < rowInfo.length; j++) {
-                table[i] = rowInfo;
-            }
-             styleDisplay = 'none';
-        }
-        for (var k = 1; k < table.length; k++) {
-            colInfo[k] = table[k][column];
+        }else{
+          OutputElem.value = C;
+          OutputElem.innerHTML = C;
         }
 
-      //  console.log(table);
-        //runs through the string that user enters
-        for (var s = 1; s < table.length; s++) {
+      };
 
-          //  console.log(strData);
-            var str = colInfo[s].toLowerCase();
+      //turn the innerHTML of any element into a clock
+      TUJS.AddClock = function(elem) {
+        var tag = TUJS.E(elem);
+        var now = new Date();
+        tag.innerHTML = now.toLocaleTimeString();
+      };
 
-            if (str.indexOf(strData) >= 0) {
-              //  console.log("Found");
-                styleDisplay = '';
-            }
-            else {
-              //  console.log("Didnt find");
-                styleDisplay = 'none';
-            }
-            tblData.rows[s].style.display = styleDisplay;
+      //bool if the obj is found inside the Element(elem) is in passed array
+      TUJS.Contains = function(elem, obj){
+        var a = TUJS.E(elem).innerHTML;
+        var i = a.length;
+        while (i--) {
+          console.log(a[i]);
+          if (a[i] === obj) {
+            console.log('true');
+            return true;
+          }
         }
-    };
+        console.log('false');
+        return false;
+      };
 
-    ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>End of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-    //call things on load
-    return TUJS;
-  }
+      //will return an array of the window size of the screen you are on
+      TUJS.getWindowSize = function() {
+        var myWidth = 0, myHeight = 0;
 
-  if (typeof(TUJS) === 'undefined') {
+        if( typeof( window.innerWidth ) == 'number' ) {
+          //everything outside of IE
+          myWidth = window.innerWidth;
+          myHeight = window.innerHeight;
+        } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+          //IE 6+ in 'standards compliant mode'... whatever that means
+          myWidth = document.documentElement.clientWidth;
+          myHeight = document.documentElement.clientHeight;
+        } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+          //IE 4 compatible... blow it up cause you should never use it
+          myWidth = document.body.clientWidth;
+          myHeight = document.body.clientHeight;
+        }
+        return [ myWidth, myHeight ];
+      };
 
-    window.TUJS = define_TUJS();
-  }
 
-})(window);
+      //returns the difference of two dates (in Days) as inputs
+      TUJS.DateDifference = function(date1, date2, otpt){
+        otpt = TUJS.E(otpt);
+        date1 = TUJS.E(date1).value;
+        date2 = TUJS.E(date2).value;
+
+        date1 = Date.parse(date1);
+        date2 = Date.parse(date2);
+        var timeDiff = Math.abs(date2 - date1);
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+        otpt.innerHTML = diffDays + " Days";
+      };
+
+      //object call to build a table object form
+      //the tableController js page
+      TUJS.tableController = function() {
+        var tbl = new Table();
+        return tbl;
+      };
+
+      ///>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>End of library<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+      //call things on load
+      return TUJS;
+    }
+
+    if (typeof(TUJS) === 'undefined') {
+
+      window.TUJS = define_TUJS();
+    }
+
+  })(window);
--- tableController.js
+++ tableController.js
@@ -18,6 +18,54 @@ var Table = function () {
    //takes in
    ColumnToList: function(){
      //
+   },
+
+   //Armond, Kyler's, and Rob's gridview auto filter
+   SearchGridview: function(strKey, strGV, column) {
+       column++;
+       //string of what is typed in
+       var strData = strKey.value.toLowerCase().split(" ");
+       // console.log(strData);
+
+       //raw data from table
+       var tblData = document.getElementById(strGV);
+       var table = [];
+       var colInfo = [];
+       var styleDisplay = "";
+       // i is each row in the table.
+       for (var i = 0; i < tblData.rows.length; i++) {
+
+           //all info in row[i]
+           var rowData = tblData.rows[i].innerHTML;
+
+           //splits row and makes arrays ex instead of <td>a</td>....
+           var rowInfo = rowData.split("<td>");
+           for (var j = 1; j < rowInfo.length; j++) {
+               table[i] = rowInfo;
+           }
+            styleDisplay = 'none';
+       }
+       for (var k = 1; k < table.length; k++) {
+           colInfo[k] = table[k][column];
+       }
+
+     //  console.log(table);
+       //runs through the string that user enters
+       for (var s = 1; s < table.length; s++) {
+
+         //  console.log(strData);
+           var str = colInfo[s].toLowerCase();
+
+           if (str.indexOf(strData) >= 0) {
+             //  console.log("Found");
+               styleDisplay = '';
+           }
+           else {
+             //  console.log("Didnt find");
+               styleDisplay = 'none';
+           }
+           tblData.rows[s].style.display = styleDisplay;
+       }
    }
 
   };
